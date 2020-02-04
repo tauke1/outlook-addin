5f2f6e6413eda47432d9b8126b58fcbacd735631
Tauke Abdrakhmanov <tauke1994@gmail.com>
2020-02-04 15:28:14 +0600

    add hint

--- AzureDevopsPlugin/Controls/ChooseUserControl.Designer.cs
+++ AzureDevopsPlugin/Controls/ChooseUserControl.Designer.cs
@@ -32,23 +32,26 @@
             this.newWorkItemBtn = new System.Windows.Forms.Button();
             this.addCommentBtn = new System.Windows.Forms.Button();
             this.workItemGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.workItemGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // userLabel
             // 
             this.userLabel.AutoSize = true;
-            this.userLabel.Location = new System.Drawing.Point(12, 24);
+            this.userLabel.Location = new System.Drawing.Point(32, 57);
+            this.userLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.userLabel.Name = "userLabel";
-            this.userLabel.Size = new System.Drawing.Size(251, 13);
+            this.userLabel.Size = new System.Drawing.Size(668, 32);
             this.userLabel.TabIndex = 0;
             this.userLabel.Text = "One or more work item found by current mail subject";
             // 
             // newWorkItemBtn
             // 
-            this.newWorkItemBtn.Location = new System.Drawing.Point(29, 389);
+            this.newWorkItemBtn.Location = new System.Drawing.Point(77, 928);
+            this.newWorkItemBtn.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.newWorkItemBtn.Name = "newWorkItemBtn";
-            this.newWorkItemBtn.Size = new System.Drawing.Size(108, 23);
+            this.newWorkItemBtn.Size = new System.Drawing.Size(288, 55);
             this.newWorkItemBtn.TabIndex = 1;
             this.newWorkItemBtn.Text = "New Work Item";
             this.newWorkItemBtn.UseVisualStyleBackColor = true;
@@ -56,9 +59,10 @@
             // 
             // addCommentBtn
             // 
-            this.addCommentBtn.Location = new System.Drawing.Point(155, 389);
+            this.addCommentBtn.Location = new System.Drawing.Point(413, 928);
+            this.addCommentBtn.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.addCommentBtn.Name = "addCommentBtn";
-            this.addCommentBtn.Size = new System.Drawing.Size(108, 23);
+            this.addCommentBtn.Size = new System.Drawing.Size(288, 55);
             this.addCommentBtn.TabIndex = 2;
             this.addCommentBtn.Text = "Add Comment";
             this.addCommentBtn.UseVisualStyleBackColor = true;
@@ -72,30 +76,44 @@
             this.workItemGridView.BackgroundColor = System.Drawing.Color.White;
             this.workItemGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.workItemGridView.ColumnHeadersVisible = false;
-            this.workItemGridView.Location = new System.Drawing.Point(15, 59);
+            this.workItemGridView.Location = new System.Drawing.Point(40, 141);
+            this.workItemGridView.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.workItemGridView.MultiSelect = false;
             this.workItemGridView.Name = "workItemGridView";
             this.workItemGridView.ReadOnly = true;
             this.workItemGridView.RowHeadersVisible = false;
+            this.workItemGridView.RowHeadersWidth = 102;
             this.workItemGridView.ShowCellErrors = false;
             this.workItemGridView.ShowEditingIcon = false;
             this.workItemGridView.ShowRowErrors = false;
-            this.workItemGridView.Size = new System.Drawing.Size(248, 302);
+            this.workItemGridView.Size = new System.Drawing.Size(661, 720);
             this.workItemGridView.TabIndex = 3;
             this.workItemGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.workItemGridView_CellDoubleClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(34, 870);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(707, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "double click on row to open work item in azure devops_";
+            // 
             // ChooseUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.workItemGridView);
             this.Controls.Add(this.addCommentBtn);
             this.Controls.Add(this.newWorkItemBtn);
             this.Controls.Add(this.userLabel);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "ChooseUserControl";
-            this.Size = new System.Drawing.Size(281, 422);
+            this.Size = new System.Drawing.Size(749, 1006);
             ((System.ComponentModel.ISupportInitialize)(this.workItemGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -108,5 +126,6 @@
         private System.Windows.Forms.Button newWorkItemBtn;
         private System.Windows.Forms.Button addCommentBtn;
         private System.Windows.Forms.DataGridView workItemGridView;
+        private System.Windows.Forms.Label label1;
     }
 }
