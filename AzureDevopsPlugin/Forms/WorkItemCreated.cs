f65459d08065b7761836075ceaf21237919c891d
Tauke Abdrakhmanov <tauke1994@gmail.com>
2020-02-10 19:53:15 +0600

    new fix

--- AzureDevopsPlugin/Forms/WorkItemCreated.cs
+++ AzureDevopsPlugin/Forms/WorkItemCreated.cs
@@ -33,6 +33,7 @@ namespace AzureDevopsPlugin.Forms
                 {
                 }
             }
+            this.Close();
         }
 
         private void okButton_Click(object sender, EventArgs e)
